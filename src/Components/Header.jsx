a5e98af1ac2bfd3d58d717d820e7bf3a373d31e4
Cornerstone Ephraim <fortunecornerstone@gmail.com>
2022-06-22 18:06:02 +0100

    Updated UI

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import "../styles/header.css"
 export default function Header() {
     return (
         <div className="Header">
-            <h1>
+            <h1 className='header_text'>
                 To-do List
             </h1>
         </div>
--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -3,37 +3,44 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEdit, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import "../styles/todo.css";
 
+import Moment from "moment";
+
 export default function Todo({ todo, setTodo, setEditTodo }) {
-	const editList = ({ id }) => {
-		const findTodo = todo.find((todo) => todo.id === id);
-		setEditTodo(findTodo);
-	};
+  const editList = ({ id }) => {
+    const findTodo = todo.find((todo) => todo.id === id);
+    setEditTodo(findTodo);
+  };
 
-	const deleteTodo = ({ id }) => {
-		setTodo(todo.filter((todo) => todo.id !== id));
-	};
+  const deleteTodo = ({ id }) => {
+    setTodo(todo.filter((todo) => todo.id !== id));
+  };
 
-	return (
-		<div className="messageArea">
-			{" "}
-			{todo.map((todo) => (
-				<div className="todo-list" key={todo.id}>
-					<input
-						type="text"
-						value={todo.title}
-						className={` list ${todo.completed ? "complete" : ""}`}
-						onChange={(event) => event.preventDefault()}
-					/>{" "}
-					<div className="eod">
-						<button className="edit-btn" onClick={() => editList(todo)}>
-							<FontAwesomeIcon icon={faEdit} />{" "}
-						</button>{" "}
-						<button className="delete-btn" onClick={() => deleteTodo(todo)}>
-							<FontAwesomeIcon icon={faTrashAlt} />{" "}
-						</button>{" "}
-					</div>{" "}
-				</div>
-			))}{" "}
-		</div>
-	);
+  const date = Moment().format("DD/MM/YYYY");
+
+  return (
+    <div className="messageArea">
+      {" "}
+      {todo.map((todo) => (
+        <div className="todo-list" key={todo.id}>
+          <div className="list_holder">
+            <p className={` list ${todo.completed ? "complete" : ""}`}>
+              {todo.title}
+            </p>
+          </div>
+          <div className="date_holder">
+            {" "}
+            <p className="date"> {date} </p>
+            <div className="eod">
+              <button className="edit-btn" onClick={() => editList(todo)}>
+                <FontAwesomeIcon icon={faEdit} />{" "}
+              </button>{" "}
+              <button className="delete-btn" onClick={() => deleteTodo(todo)}>
+                <FontAwesomeIcon icon={faTrashAlt} />{" "}
+              </button>{" "}
+            </div>{" "}
+          </div>
+        </div>
+      ))}{" "}
+    </div>
+  );
 }
