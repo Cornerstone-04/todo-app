3eb0bb16c97ca0fd39e6e1329c08ac6ca3a9650d
Cornerstone <fortunecornerstone@gmail.com>
2022-11-23 10:40:33 +0100

    feat: Add scroll up function

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,36 @@ import React, { useState, useEffect } from "react";
 import "./index.css";
 import { Header, Form, List } from "./Components";
 
-function App() {
+const App = () => {
   const initialState = JSON.parse(localStorage.getItem("todo")) || [];
   const [input, setInput] = useState("");
   const [todo, setTodo] = useState(initialState);
   const [editTodo, setEditTodo] = useState(null);
+  const [displayArrow, setDisplayArrow] = useState(false);
+
+  const scrollUp = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+  };
+
+  const listenToScroll = () => {
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      setDisplayArrow(true);
+    } else {
+      setDisplayArrow(false);
+    }
+  };
 
   useEffect(() => {
     localStorage.setItem("todo", JSON.stringify(todo));
+    window.addEventListener("scroll", listenToScroll);
+    return () => window.removeEventListener("scroll", listenToScroll);
   }, [todo]);
 
   return (
-    <div className="App">
+    <div className="flex flex-col justify-center items-center w-full text-center mx-auto my-0 bg-[#00040f] relative">
       <Header />
       <Form
         input={input}
@@ -24,8 +42,13 @@ function App() {
         setEditTodo={setEditTodo}
       />
       <List todo={todo} setTodo={setTodo} setEditTodo={setEditTodo} />
+      {displayArrow && (
+        <button className="fixed bottom-[15px] right-[15px]" onClick={scrollUp}>
+          ⬆️
+        </button>
+      )}
     </div>
   );
-}
+};
 
 export default App;
